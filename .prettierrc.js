5e7cdde74610212a8bc6c0cd1f044c8e9d1e0d01
wxfeiang <wxfeiang@qq.com>
2025-03-06 22:46:17 +0800

    feat: init

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    //使用单引号还是双引号，选择true，使用单引号；
+    "singleQuote": true,
+    //tab是空格的情况下，是几个空格，选择2个；
+    "tabWidth": 2,
+    //箭头函数的参数总是使用括号
+    "arrowParens": "always",
+    //对象字面量中的括号前后有空格。
+    "bracketSpacing": true,
+    //保持原有的换行方式
+    "proseWrap": "preserve",
+    //在多行输入的尾逗号是否添加，设置为 `all`
+    "trailingComma": "all",
+    //在 JSX 中使用双引号，设置为false
+    "jsxSingleQuote": false,
+    //当行字符的长度，推荐80，也有人喜欢100或者120；
+    "printWidth": 100,
+    //自动识别并使用合适的行结束符
+    "endOfLine": "auto"
+}
---
+++ .stylelintrc.js
@@ -0,0 +1,35 @@
+/** @type {import('stylelint').Config} */
+
+export default {
+    // 从标准配置中继承规则
+    extends: [
+        "stylelint-config-standard",
+        "stylelint-config-rational-order",
+        "prettier",
+    ],
+    plugins: [
+        "stylelint-declaration-block-no-ignored-properties",
+        "stylelint-prettier",
+    ],
+    // 规则配置
+    rules: {
+        // 禁用注释前的空行规则
+        "comment-empty-line-before": null,
+        // 禁用声明前的空行规则
+        "declaration-empty-line-before": null,
+        // 指定函数名的大小写为小写
+        "function-name-case": "lower",
+        // 禁用选择器特异性递减规则
+        "no-descending-specificity": null,
+        // 禁用无效的双斜杠注释规则
+        "no-invalid-double-slash-comments": null,
+        // 指定规则前需要空行
+        "rule-empty-line-before": "always",
+        //stylelint-declaration-block-no-ignored-properties 用于提示我们写的矛盾样式
+        "plugin/declaration-block-no-ignored-properties": true,
+    },
+
+    // 忽略检查的文件或文件夹
+    ignoreFiles: ["node_modules/**/*", "build/**/*"],
+};
+
