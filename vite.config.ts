fd0a6634cc4ce4cf764dc1d8bf526359f14efb6b
wxfeiang <wxfeiang@qq.com>
2025-03-12 14:32:17 +0800

    feat: config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), UnoCSS()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 })
