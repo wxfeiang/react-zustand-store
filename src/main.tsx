0c40feb505413c2dade5f5fd2af4a79ca0c00dfb
wxfeiang <wxfeiang@qq.com>
2025-03-20 16:41:17 +0800

    fix:  reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import '@ant-design/v5-patch-for-react-19';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '@unocss/reset/tailwind.css';
+import 'rest-css'
 
 import 'virtual:uno.css';
 import 'amfe-flexible';
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Button, message } from 'antd';
 
 const Main: React.FC = () => {
   return (
     <>
-      <Layout className="w-100vw h-100vh">
+      <Layout className="w-100vw h-100vh bg-#000 ">
         <div className="grid grid-cols-3 gap-20px h-100%">
-          <div className="bd-1px_#1890ff h-100%">01</div>
+          <div className="bd-1px_#1890ff h-100%">
+            <Button type='primary' onClick={() => {
+              message.info('测试动画弹框')
+            }}>测试动画弹框</Button>
+            <Button type="primary">Primary Button</Button>
+          </div>
           <div className="bd-1px_#1890ff h-100%">01</div>
           <div className="bd-1px_#1890ff h-100%">01</div>
         </div>
